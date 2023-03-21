c5cb76f8658ffab06070473758239357d72f3789
kot8585 <ansgywjd321@gmail.com>
2023-03-21 15:44:04 +0900

    14.16 : PageItemComponent를 만들어서 공통되는 스타일링 속성(아이템 삭제) 가지도록 하기
    - 아이템 삭제버튼을 가지고 싶지 않은 경우 PageItemComponent를 사용하지 않으면 되기때문에 재사용성이 높다.
    - PageComponent와 PageItemComponent 둘다 자식 컴포넌트를 자기 안에 추가 할 수 있는 클래스이므로 Composable라는 인터페이스 생성

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,29 @@
+import { Component } from "./components/component.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
-import { PageComponent } from "./components/page/page.js";
+import { Composable, PageComponent } from "./components/page/page.js";
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
 
     const imageComponent = new ImageComponent("../assets/sample.png", "Coding");
-    imageComponent.attachTo(appRoot, "beforeend");
+    this.page.addChild(imageComponent);
 
     const noteComponent = new NoteComponent("typescipt", "재밌어져라");
-    noteComponent.attachTo(appRoot, "beforeend");
+    this.page.addChild(noteComponent);
 
     const todoComponent = new TodoComponent("Todo", "음료수 마시기");
-    todoComponent.attachTo(appRoot, "beforeend");
+    this.page.addChild(todoComponent);
 
     const videoComponent = new VideoComponent(
       "유튜브 영상",
       "https://youtu.be/d14cQHBtZc4"
     );
-    videoComponent.attachTo(appRoot, "beforeend");
+    this.page.addChild(videoComponent);
   }
 }
 
--- src/components/page/item/note.ts
+++ src/components/page/item/note.ts
@@ -4,7 +4,7 @@ export class NoteComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, body: string) {
     super(`
       <section class="note">
-          <h3 class="note__title"></h3>
+          <h2 class="note__title"></h2>
           <p class="note__body"></p>
       </section>`);
     const titleElement = this.element.querySelector(
